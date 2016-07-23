732406f41ac6a7389fe87a7b39f92e7987df1b4c
Bryan Gebhardt <bryan.gebhardt@gmail.com>
2016-07-23 09:41:28 -0700

    example wdwJS output

---
+++ wdwJS_example.js
@@ -0,0 +1,17 @@
+// example output from the wdwJS API
+var rideExample = [{
+  "id": "18440617",
+  "name": "Disney \"Pete's Dragon\" Sneak Peek",
+  "waitTime": 0,
+  "active": true,
+  "status": "Operating",
+  "fastPass": false,
+  "schedule": {
+    "openingTime": "2016-07-17T08:00:00-07:00",
+    "closingTime": "2016-07-17T22:00:00-07:00",
+    "special": [],
+    "type": "Operating"
+  }
+}];
+
+console.log(rideExample[0]);
