defdd907289d4ca5e9d4be4f19196c4db05c9b34
Bryan Gebhardt <bryan.gebhardt@gmail.com>
2016-09-26 18:08:42 -0700

    Created a much simpler script after adding the quoting to app.js.

--- clean_data.sh
+++ clean_data.sh
@@ -1,29 +1,20 @@
 #!/usr/bin/env bash
 
 # script to clean up and combine the data files collected with app.js
+# pass in the date of the data you are combining (or the name you want of the final file)
 
 #FILE="2016-07-18.csv"
 FILE=$1
 echo "## processing $FILE..."
 
-# command to fix commas inplace with backup
-# it still leaves an empty extra Goofy's sky school due to the <CR> in that item. :P
-sed -i.bak -e 's/Monsters,/Monsters/g' -e 's/Fire Engine,/Fire Engine/g' -e 's/Streetcars,/Streetcars/g' -e 's/Carriage,/Carriage/g' -e 's/Omnibus,/Omnibus/g' -e 's/^,/California Adventure,Goofys Sky School,/g' *.csv
-
-# move the back up files
-mkdir bak
-mv *.bak bak
-
-
 # combine all the files
 cat *.csv > $FILE.tmp1
 
 # clean a few things
-grep -v "California Adventure,Goofy's Sky School" $FILE.tmp1 > $FILE.tmp2 # remove bad goofy's sky school
-grep -v park $FILE.tmp2 > $FILE.tmp3  # removes header
+grep -v park $FILE.tmp1 > $FILE.tmp2  # removes header
 
 # add a header
-cat ../header $FILE.tmp3 > $FILE
+cat ../header $FILE.tmp2 > $FILE.csv
 
 mkdir figures # for saving graphs later
 
