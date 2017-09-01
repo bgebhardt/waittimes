defdac1dead9789c78389559cae4b9c9a500fd6a
Bryan Gebhardt <bryan.gebhardt@gmail.com>
2017-09-01 07:35:53 -0700

    Script now places wait times in a directory for the day for easier file management.

--- run_disney.sh
+++ run_disney.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 # simple script you can put in crontab to grab wait times.
-/usr/local/bin/node /Users/bryan/code/personal/waittimes/app.js >/Users/bryan/code/personal/waittimes/disney_out/`date +%F-%T`.csv 2>>/tmp/disney_stderr.log
+
+# create directory for day
+mkdir -p /Users/bryan/code/personal/waittimes/disney_out/`date +%F`
+
+# grab wait times
+/usr/local/bin/node /Users/bryan/code/personal/waittimes/app.js >/Users/bryan/code/personal/waittimes/disney_out/`date +%F`/`date +%F-%T`.csv 2>>/tmp/disney_stderr.log
